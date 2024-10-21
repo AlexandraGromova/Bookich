e70c1a1bda6995d9a80db1afdd138c5a70058edc
Sasha Gromova <sasadanilina2@gmail.com>
2024-10-21 23:29:29 +0600

    add main screen

---
+++ Bookich/Logic/VM/MainVM.swift
@@ -0,0 +1,12 @@
+//
+//  MainVM.swift
+//  Bookich
+//
+//  Created by Sasha on 21.10.2024.
+//
+
+import Foundation
+
+class MainVM {
+    
+}
---
+++ Bookich/UI/MainScreen.swift
@@ -0,0 +1,22 @@
+//
+//  MainScreen.swift
+//  Bookich
+//
+//  Created by Sasha on 21.10.2024.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+class MainScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    func setup() {
+        
+    }
+}
